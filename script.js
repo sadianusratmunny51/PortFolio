34cb725b448d9ba10740203895904add3e53f8e8
sadianusratmunny51 <sadiamunny51@gmail.com>
2026-09-09 20:31:44 +0600

    Update portfolio

---
+++ script.js
@@ -0,0 +1,131 @@
+
+
+//image animation
+const aboutImg = document.querySelector('.about-img');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      aboutImg.classList.add('animate');
+    }
+  });
+}, { threshold: 0.5 });
+
+observer.observe(aboutImg);
+
+
+//hover effecr  percentage calculation
+document.querySelectorAll('.inner-content').forEach(box => {
+  const percent = parseInt(box.getAttribute('data-percent'), 10);
+  const progressBar = box.querySelector('.progress-bar');
+  const percentText = box.querySelector('.percent-text');
+  let countInterval;
+
+  box.addEventListener('mouseenter', () => {
+    let current = 0;
+    clearInterval(countInterval);
+    percentText.textContent = "0%";
+    progressBar.style.width = percent + '%';
+
+    countInterval = setInterval(() => {
+      if (current < percent) {
+        current++;
+        percentText.textContent = current + "%";
+      } else {
+        clearInterval(countInterval);
+      }
+    }, 15);
+  });
+
+  box.addEventListener('mouseleave', () => {
+    clearInterval(countInterval);
+    percentText.textContent = '';
+    progressBar.style.width = '0%';
+  });
+});
+
+
+//typing effect 
+document.addEventListener("DOMContentLoaded", () => {
+  const text = "Software Engineering Student";
+  const typingElement = document.getElementById("typing-text");
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      typingElement.textContent += text.charAt(index);
+      index++;
+      setTimeout(type, 150); 
+    }
+  }
+
+  typingElement.textContent = "";
+  type();
+});
+
+
+// Using id
+
+document.getElementById('stopstalk-btn').addEventListener('click', () => {
+    window.open('https://www.stopstalk.com/', '_blank');
+});
+
+
+document.getElementById('contents-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51', '_blank');
+});
+
+document.getElementById('web-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51/Web_Projects', '_blank');
+});
+
+
+document.getElementById('game-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51/SWE224GAME', '_blank');
+});
+
+document.getElementById('app-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51/SWE250_Project', '_blank');
+});
+
+
+document.getElementById('view-app-ui-btn').addEventListener('click', () => {
+    window.open('', '_blank');
+});
+
+document.getElementById('tracease-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51/SWE250_Project', '_blank');
+});
+
+
+document.getElementById('soaring-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51/SWE224GAME', '_blank');
+});
+
+
+document.getElementById('').addEventListener('click', () => {
+    window.open('', '_blank');
+});
+
+document.getElementById('tracease-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51/SWE250_Project', '_blank');
+});
+
+
+document.getElementById('soaring-btn').addEventListener('click', () => {
+    window.open('https://github.com/sadianusratmunny51/SWE224GAME', '_blank');
+});
+
+
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', function(event) {
+
+  event.preventDefault();
+
+
+  alert('Message Sent Successfully');
+});
+
+
+
